a49ebbb8f3181061c3bb2bc669da8d46b3a26d2b
PavelCp <pavelcapet@gmail.com>
2017-03-07 10:21:43 +0200

    Add files via upload

---
+++ src/test/java/com/testng/AppTest.java
@@ -0,0 +1,17 @@
+package com.testng;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void AppTest(String testName )
+    {
+        System.out.println("Test1 run");
+    }
+
+}
